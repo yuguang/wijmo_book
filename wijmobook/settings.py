a037276fabeec455230302172de5e47c800937c0
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-02 17:39:36 -0500

    organize files for use with django static file management

--- wijmobook/settings.py
+++ wijmobook/settings.py
@@ -1,4 +1,6 @@
 # Django settings for wijmobook project.
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'E:/Users/Yuguang/'
+STATIC_ROOT = '/home/yuguang/www/wijmobook/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'E:/Users/Yuguang/wijmo_book',
+    os.path.join(BASE_DIR, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -112,7 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'E:/Users/Yuguang/wijmo_book',
+    os.path.join(BASE_DIR, "templates"),
 )
 
 INSTALLED_APPS = (
