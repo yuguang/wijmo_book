4dc9eaee8554204f602e5fa8869e5bed8359bb02
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-03 07:51:20 -0500

    simplify form and use django's form API

--- static/js/main.js
+++ static/js/main.js
@@ -8,21 +8,6 @@ $('#widget-explorer').click(function() {
 })
 $('body').removeClass('invisible');
 if ($('#contact').length) {
-    var messageTypes = $.map(["Comment", "Review", "Errata", "Other"], function (type) {
-        return {label: type, value: type}
-    });
-    //Create ViewModel
-    var ViewModel = function () {
-        this.subject = ko.observable('');
-        this.body = ko.observable('');
-        this.messageType = ko.observable('Comment');
-        this.submit = function () {
-            socket.emit('message', ko.toJSON(viewModel));
-        }
-    };
-    $('#subject,#body').wijtextbox();
+    $('textarea,input').wijtextbox();
     $('button').button();
-
-    var viewModel = new ViewModel();
-    ko.applyBindings(viewModel);
 }
