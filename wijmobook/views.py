01d6f082b51b8e91360d75af953de1d93046eb95
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-03 09:19:46 -0500

    rename image files

--- wijmobook/views.py
+++ wijmobook/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 import settings, os, glob
 
 def gallery(request):
-    files = ['img/' + os.path.basename(file) for file in glob.glob(os.path.join(settings.BASE_DIR, 'static/img/*.png'))]
+    files = []
+    for file in glob.glob(os.path.join(settings.BASE_DIR, 'static/img/*.png')):
+        files.append({
+            'path': 'img/' + os.path.basename(file),
+            'title': os.path.basename(file)
+        })
     return render(request, 'gallery.html', { 'images': files })
