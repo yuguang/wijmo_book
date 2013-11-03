b0bc791dec3381b1464c1ab96416467dcc089679
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-02 19:17:40 -0500

    display images in a folder

--- wijmobook/views.py
+++ wijmobook/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+import settings, os, glob
 
 def home(request):
     return render(request, 'index.html')
 
 def gallery(request):
-    return render(request, 'gallery.html')
+    files = ['img/' + os.path.basename(file) for file in glob.glob(os.path.join(settings.BASE_DIR, 'static/img/*.png'))]
+    return render(request, 'gallery.html', { 'images': files })
