09fa5cbb4045cd5330e606dd3b1023da809c32b8
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-02 21:16:12 -0500

    wijmo form decorator to make form modern

--- static/js/main.js
+++ static/js/main.js
@@ -7,3 +7,22 @@ $('#widget-explorer').click(function() {
     $('#dialog').wijdialog({ width: 840, height: 640, contentUrl: 'http://wijmo.com/demo/explore/?showall=true', autoOpen: true});
 })
 $('body').removeClass('invisible');
+if ($('#contact').length) {
+    var messageTypes = $.map(["Comment", "Review", "Errata", "Other"], function (type) {
+        return {label: type, value: type}
+    });
+    //Create ViewModel
+    var ViewModel = function () {
+        this.subject = ko.observable('');
+        this.body = ko.observable('');
+        this.messageType = ko.observable('Comment');
+        this.submit = function () {
+            socket.emit('message', ko.toJSON(viewModel));
+        }
+    };
+    $('#subject,#body').wijtextbox();
+    $('button').button();
+
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+}
