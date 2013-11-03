91e26fd4011215421d78605002fc3232f7833773
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-02 19:45:55 -0500

    render to template using a class based view

--- wijmobook/urls.py
+++ wijmobook/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'wijmobook.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), 'home'),
     url(r'^screenshots/', 'wijmobook.views.gallery', name='gallery'),
-    # url(r'^wijmobook/', include('wijmobook.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- wijmobook/views.py
+++ wijmobook/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 import settings, os, glob
 
-def home(request):
-    return render(request, 'index.html')
-
 def gallery(request):
     files = ['img/' + os.path.basename(file) for file in glob.glob(os.path.join(settings.BASE_DIR, 'static/img/*.png'))]
     return render(request, 'gallery.html', { 'images': files })
