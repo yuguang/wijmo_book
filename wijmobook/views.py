c3a07e67c9e9b895d60411522dc56c6f14f768e0
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-03 09:46:12 -0500

    generate titles for each image

--- wijmobook/views.py
+++ wijmobook/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-import settings, os, glob
+import settings, os, glob, string
+
+def underscore_to_camelcase(s):
+    return string.capwords(s, '_').replace('_', ' ')
 
 def gallery(request):
     files = []
     for file in glob.glob(os.path.join(settings.BASE_DIR, 'static/img/*.png')):
+        base_file = os.path.basename(file)
         files.append({
-            'path': 'img/' + os.path.basename(file),
-            'title': os.path.basename(file)
+            'path': 'img/' + base_file,
+            'title': underscore_to_camelcase(os.path.splitext(base_file)[0])
         })
     return render(request, 'gallery.html', { 'images': files })
