571358e13c611bdee83d748aec4c1433a2264605
Yuguang <zh.yuguang.cn@gmail.com>
2013-11-02 20:29:39 -0500

    widget explorer with wijmo's dialog widget to load external content from wijmo demo page

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,7 @@ $('#tree').wijtree();
 $('#gallery').wijgallery({
     thumbsDisplay: 3
 });
-$('body').removeClass('invisible')
+$('#widget-explorer').click(function() {
+    $('#dialog').wijdialog({ width: 840, height: 640, contentUrl: 'http://wijmo.com/demo/explore/?showall=true', autoOpen: true});
+})
+$('body').removeClass('invisible');
